75f3346a6df8c6c522bab776b9e59410a0fc6db5
David Trevelyan <david.trevelyan@gmail.com>
2023-10-31 17:25:26 +0000

    Add (de)allocation radsan tests

--- compiler-rt/lib/radsan/tests/radsan_test.cpp
+++ compiler-rt/lib/radsan/tests/radsan_test.cpp
@@ -1,15 +1,83 @@
 #include "gtest/gtest.h"
 
-auto vec = std::vector<float> (300);
+#include <array>
 
-[[clang::realtime]] int unsafeRealtimeFunction() 
-{
-  for (auto i = 0; i < 10; ++i)
-    vec.push_back(static_cast<float> (i));
-  return vec.size();
+using namespace testing;
+
+namespace {
+template <typename Function>
+[[clang::realtime]] void realtimeInvoke(Function &&func) {
+  std::forward<Function>(func)();
+}
+template <typename Function> void expectRealtimeDeath(Function &&func) {
+  EXPECT_DEATH(realtimeInvoke(std::forward<Function>(func)), "radsan");
 }
 
-TEST (RealtimeSanitizer, newCausesDeath)
-{
-    EXPECT_DEATH (unsafeRealtimeFunction(), "radsan");
+template <typename Function> void expectNonrealtimeSurvival(Function &&func) {
+  std::forward<Function>(func)();
+}
+} // namespace
+
+TEST(TestRealtimeSanitizer, mallocDiesWhenRealtime) {
+  auto func = []() { EXPECT_NE(nullptr, malloc(1)); };
+  expectRealtimeDeath(func);
+  expectNonrealtimeSurvival(func);
+}
+
+TEST(TestRealtimeSanitizer, reallocDiesWhenRealtime) {
+  auto *ptr_1 = malloc(1);
+  auto func = [ptr_1]() { EXPECT_NE(nullptr, realloc(ptr_1, 8)); };
+  expectRealtimeDeath(func);
+  expectNonrealtimeSurvival(func);
+}
+
+TEST(TestRealtimeSanitizer, reallocfDiesWhenRealtime) {
+  auto *ptr_1 = malloc(1);
+  auto func = [ptr_1]() { EXPECT_NE(nullptr, reallocf(ptr_1, 8)); };
+  expectRealtimeDeath(func);
+  expectNonrealtimeSurvival(func);
+}
+
+TEST(TestRealtimeSanitizer, vallocDiesWhenRealtime) {
+  auto func = []() { EXPECT_NE(nullptr, valloc(4)); };
+  expectRealtimeDeath(func);
+  expectNonrealtimeSurvival(func);
+}
+
+TEST(TestRealtimeSanitizer, alignedAllocDiesWhenRealtime) {
+  auto func = []() { EXPECT_NE(nullptr, aligned_alloc(16, 32)); };
+  expectRealtimeDeath(func);
+  expectNonrealtimeSurvival(func);
+}
+
+/*
+    free_sized and free_aligned_sized (both C23) are not yet supported
+*/
+TEST(TestRealtimeSanitizer, freeDiesWhenRealtime) {
+  auto *ptr_1 = malloc(1);
+  auto *ptr_2 = malloc(1);
+  expectRealtimeDeath([ptr_1]() { free(ptr_1); });
+  expectNonrealtimeSurvival([ptr_2]() { free(ptr_2); });
+
+  // Prevent malloc/free pair being optimised out
+  ASSERT_NE(nullptr, ptr_1);
+  ASSERT_NE(nullptr, ptr_2);
+}
+
+TEST(TestRealtimeSanitizer, vectorPushBackAllocationDiesWhenRealtime) {
+  auto vec = std::vector<float>{};
+  auto func = [&vec]() { vec.push_back(0.4f); };
+  expectRealtimeDeath(func);
+  ASSERT_EQ(0u, vec.size());
+  expectNonrealtimeSurvival(func);
+  ASSERT_EQ(1u, vec.size());
+}
+
+TEST(TestRealtimeSanitizer, destructionOfObjectOnHeapDiesWhenRealtime) {
+  auto obj = std::make_unique<std::array<float, 256>>();
+  auto func = [&obj]() { obj.reset(); };
+  expectRealtimeDeath(func);
+  ASSERT_NE(nullptr, obj.get());
+  expectNonrealtimeSurvival(func);
+  ASSERT_EQ(nullptr, obj.get());
 }
--- compiler-rt/lib/radsan/tests/radsan_test_main.cpp
+++ compiler-rt/lib/radsan/tests/radsan_test_main.cpp
@@ -1,8 +1,6 @@
 #include "sanitizer_test_utils.h"
-// #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
-
   testing::GTEST_FLAG(death_test_style) = "threadsafe";
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
