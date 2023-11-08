d2092386c79a1fc18ec6b64b6a95e9e7fe0c7e73
Ali <alibarker@Alis-MacBook-Air.local>
2023-11-08 16:11:49 +0000

    Fix compiler warnings

--- llvm/lib/Transforms/Utils/CodeExtractor.cpp
+++ llvm/lib/Transforms/Utils/CodeExtractor.cpp
@@ -961,6 +961,7 @@ Function *CodeExtractor::constructFunction(const ValueSet &inputs,
       case Attribute::MustProgress:
       case Attribute::NoProfile:
       case Attribute::SkipProfile:
+      case Attribute::Realtime:
         break;
       // These attributes cannot be applied to functions.
       case Attribute::Alignment:
